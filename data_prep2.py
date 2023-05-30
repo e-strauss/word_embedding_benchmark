5d45e41a20521733306d29413fcbdd28364732cc
e-strauss <92718421+e-strauss@users.noreply.github.com>
2023-05-30 14:17:13 +0200

    Initial commit

--- data_prep2.py
+++ data_prep2.py
@@ -9,18 +9,18 @@ def expand_list(input_list, factor):
 if len(sys.argv) != 3:
     print("please specify shrinking and extension factor, e.g. python3 data_prep.py 2 10")
 else:
-    with open("data/words.csv", 'r', newline='') as file:
+    with open("data_default/words.csv", 'r', newline='') as file:
         reader = csv.reader(file)
         words = [word[0] for word in reader]
     length = int(len(words) / int(sys.argv[1]))
     words = words[:int(len(words) / int(sys.argv[1]))]
 
-    with open("data/words_dictionary.csv", 'r', newline='') as rfile:
+    with open("data_default/words_dictionary.csv", 'r', newline='') as rfile:
         with open("data/words_dictionary_{}.csv".format(sys.argv[1]), 'w', newline='') as wfile:
             for i in range(length):
                 wfile.write(rfile.readline())
 
-    with open("data/embeddings.csv", 'r', newline='') as rfile:
+    with open("data_default/embeddings.csv", 'r', newline='') as rfile:
         with open("data/embeddings_{}.csv".format(sys.argv[1]), 'w', newline='') as wfile:
             for i in range(length):
                 wfile.write(rfile.readline())
